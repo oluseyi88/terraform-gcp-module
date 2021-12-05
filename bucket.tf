129a5df7cb93cd7388a210ba2b792853955463dc
idemudia uyinmwen <idemudiauyinmwen@yahoo.com>
2021-12-05 12:41:10 -0500

    new-version

--- bucket.tf
+++ bucket.tf
@@ -1,9 +1,7 @@
 resource "google_storage_bucket" "test_bucket" {
   name          = var.bucket_name
   location      = "US"
-  force_destroy = true
-  #depends_on = [
-   # google_compute_instance.vm_instance
-  #]
+  force_destroy= true
+  versioning = true
  
 }
